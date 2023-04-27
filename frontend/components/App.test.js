6dbc9836e2f2a589bb856ccda8fcb2d8c48b13c8
dgnyngz <dgnyngz@gmail.com>
2023-04-27 17:40:40 +0300

    bitmek üzere

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,19 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import AppFunctional from "../components/AppFunctional";
+
+test("hatasız render ediliyor mu", () => {
+  render(<AppFunctional />);
+});
+test("başlangıç adım sayısı 0 mı", () => {
+  expect(screen.getByTestId("steps").textContent).toBe("0 kere ilerlediniz");
+});
+
+test("başlangıç kordinatları doru mu", () => {
+  expect(screen.getByTestId("coordinates").textContent).toBe(
+    "Koordinatlar (2, 2)"
+  );
+});
+//npm test components/App.test.js
